870f7d7d9a1d0feb387396daeab3609f4070aab9
yui <yui@yui-macbook.local>
2016-12-27 01:12:38 +0900

    Upgrade to v0.1.0

--- myip.rb
+++ myip.rb
@@ -4,15 +4,15 @@ class Myip < Formula
   homepage "https://github.com/kitsuyui/go-myip"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/kitsuyui/go-myip/releases/download/0.0.1/myip_darwin_amd64"
-    sha256 "17dac2a8d0904acfb3f250bffe30539bb8dc0fb674ab3492d764413425ad0166"
+    url "https://github.com/kitsuyui/go-myip/releases/download/0.1.0/myip_darwin_amd64"
+    sha256 "cde6ab18dabf38e6011cd26a9b03a9d08c291c2d4d7381c20d6e7dcc5e17a832"
   else
-    url "https://github.com/kitsuyui/go-myip/releases/download/0.0.1/myip_darwin_386"
-    sha256 "c95cbdc713a47232c40534021547f540ce538fb7ae51667162e2c4ab4659870d"
+    url "https://github.com/kitsuyui/go-myip/releases/download/0.1.0/myip_darwin_386"
+    sha256 "478b289bee9ff676327d85bdd568b2e0f2387aebdd0ada88ebbf385f57d48b56"
   end
 
   head "https://github.com/kitsuyui/go-myip.git"
-  version "0.0.1"
+  version "0.1.0"
 
   def install
     if Hardware::CPU.is_64_bit?
