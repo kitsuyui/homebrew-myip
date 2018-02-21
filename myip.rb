f4a5549017b5680a3387edf412bcae36aed5d732
kitsuyui <kitsuyui@kitsuyui.com>
2018-02-21 16:40:47 +0900

    v0.3.3

--- myip.rb
+++ myip.rb
@@ -4,15 +4,15 @@ class Myip < Formula
   homepage "https://github.com/kitsuyui/go-myip"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/kitsuyui/go-myip/releases/download/v0.3.0/myip_darwin_amd64"
-    sha256 "9508548c1451822b8934c857dc520da30541538721e0204f07f101fe16241d2f"
+    url "https://github.com/kitsuyui/go-myip/releases/download/v0.3.3/myip_darwin_amd64"
+    sha256 "c4ee79e55aa206033a76e1cafb46e766c14307652886eb02bfa6dd0d56968e77"
   else
-    url "https://github.com/kitsuyui/go-myip/releases/download/v0.3.0/myip_darwin_386"
-    sha256 "b3a4fa0d64b68206357781ca91601181d6960e878398faea302e81b1ed6398e2"
+    url "https://github.com/kitsuyui/go-myip/releases/download/v0.3.3/myip_darwin_386"
+    sha256 "108b3cab0b23d10bfa765b40bf5fc8a35f18c478798d548bb414549f87019169"
   end
 
   head "https://github.com/kitsuyui/go-myip.git"
-  version "v0.3.0"
+  version "v0.3.3"
 
   def install
     if Hardware::CPU.is_64_bit?
