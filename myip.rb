4ab19e13bf7c6215b850c5f3db2f123f1209466c
kitsuyui <2596972+kitsuyui@users.noreply.github.com>
2026-07-20 03:18:07 +0000

    Update myip formula

--- myip.rb
+++ myip.rb
@@ -2,14 +2,14 @@ class Myip < Formula
   homepage "https://github.com/kitsuyui/myip"
 
   stable do
-    version "v0.3.10"
+    version "v0.3.12"
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kitsuyui/myip/releases/download/v0.3.10/myip_Darwin_arm64.tar.gz"
-      sha256 "9f849345ff0fae25f2deb429597db065435fb788c82acf14de3a4930786d83ec"
+      url "https://github.com/kitsuyui/myip/releases/download/v0.3.12/myip_Darwin_arm64.tar.gz"
+      sha256 "77217f7a2ed9460657da68e05cc115c9604f3e88cb36fa819281cacf4ef8ed86"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kitsuyui/myip/releases/download/v0.3.10/myip_Darwin_x86_64.tar.gz"
-      sha256 "30d8a30d0414e7077ef11d6d303239e3512473e3f6d73b22ecfcf3daa501bb01"
+      url "https://github.com/kitsuyui/myip/releases/download/v0.3.12/myip_Darwin_x86_64.tar.gz"
+      sha256 "38c1518159a166ec70d2f2f5f59cf9a9c50ee347092d756039c6d7f336ae40fd"
     else
       odie "myip binary releases are only available for Apple Silicon and 64-bit Intel macOS"
     end
@@ -26,9 +26,5 @@ class Myip < Formula
     else
       bin.install "myip" => "myip"
     end
-  end
-
-  test do
-    system bin/"myip", "--help"
   end
 end
